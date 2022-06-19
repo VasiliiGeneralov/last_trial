e7d925de1c03af5d9621c679f4d09645df0719a3
Vasilii Generalov <vasilii.generalov@gmail.com>
2022-06-19 14:49:49 +0300

    Improve api usage
    Signed-off-by: Vasilii Generalov <vasilii.generalov@gmail.com>

--- tracklist_reader.py
+++ tracklist_reader.py
@@ -9,12 +9,14 @@ import ptp
 if __name__ == '__main__':
     client = pipeclient.PipeClient()
     client.write("SelectAll:")
-    with open('tracklist_youtube.csv', 'r') as f:
+    with open('tracklist.csv', 'r', newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
         try:
             for row in reader:
                 client.write('Select: Start={} End={}'.format(
                     ptp.colon_to_sec(row[2]), ptp.colon_to_sec(row[3])))
                 client.write('AddLabel: ')
+                client.write(
+                    'SetLabel: Label={} Text="{}"'.format(int(row[0]) - 1, row[1]))
         except csv.Error as e:
             sys.exit('file {}, line {}: {}'.format(f, reader.line_num, e))
