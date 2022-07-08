82e53323f289e49a72abd207b7b5965453b97afc
Vasilii Generalov <vasilii.generalov@gmail.com>
2022-07-08 22:56:46 +0300

    Extract common code to separate class
    Signed-off-by: Vasilii Generalov <vasilii.generalov@gmail.com>

--- tracklist_reader.py
+++ tracklist_reader.py
@@ -6,20 +6,27 @@ import pipeclient
 import ptp
 
 
+class ReadWriteSequenceHandler:
+    def __init__(self, client: pipeclient.PipeClient):
+        self.__pipe_client = client
+
+    def run(self, cmd: str):
+        self.__pipe_client.write(cmd)
+        self.__pipe_client.read()
+
+
 if __name__ == '__main__':
-    client = pipeclient.PipeClient()
-    client.write("SelectAll:")
+    client = pipeclient.PipeClient('utf-8')
+    handler = ReadWriteSequenceHandler(client)
+    handler.run('SelectAll:')
     with open('tracklist.csv', 'r', newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
         try:
             for row in reader:
-                client.write('Select: Start={} End={}'.format(
+                handler.run('Select: Start={} End={}'.format(
                     ptp.colon_to_sec(row[2]), ptp.colon_to_sec(row[3])))
-                client.read()
-                client.write('AddLabel: ')
-                client.read()
-                client.write('SetLabel: Label={} Text="{}"'.format(
+                handler.run('AddLabel: ')
+                handler.run('SetLabel: Label={} Text="{}"'.format(
                     int(row[0]) - 1, row[1]))
-                client.read()
         except csv.Error as e:
             sys.exit('file {}, line {}: {}'.format(f, reader.line_num, e))
